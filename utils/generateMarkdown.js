c13fcaf93f533a6f30b2841630bba67eda4ec2cd
pcaiozzo <paigecaiozzo@gmail.com>
2022-06-05 21:05:05 -0400

    changed some badge information

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -40,12 +40,50 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    if (license !== "no license") {
+      return `
+  [![badge](https://img.shields.io/badge/license-${license}-blue)](http://choosealicense.com/licenses/${license}/)`;
+    } else {
+      return " ";
+    }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
+  let licenseString = "";
+  switch (license) {
+    case "Apache License 2.0":
+      licenseString =
+        "The project is under [Apache](https://opensource.org/licenses/Apache-2.0) license.";
+      break;
+    case "GNU GPL v3":
+      licenseString =
+        "The project is under [GNU GPL v3](https://www.gnu.org/licenses/gpl-3.0) license.";
+      break;
+    case "ISC":
+      licenseString =
+        "The project is under [ISC](https://opensource.org/licenses/ISC) license.";
+      break;
+    case "MIT":
+      licenseString =
+        "The project is under [MIT](https://opensource.org/licenses/MIT) license.";
+      break;
+    case "no license":
+      licenseString =
+        "The project is under [no license](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/) license.";
+      break;
+    case "Mozilla":
+      licenseString =
+        "This project is under [Mozilla](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+      break;
 
+    default:
+      licenseString = "The project has no license retriction.";
+      break;
+  }
+  return licenseString;
     };
 
 // TODO: Create a function to generate markdown for README
