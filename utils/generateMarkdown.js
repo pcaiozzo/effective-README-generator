7def137ab3f1d885f4fafbbb6891d7f241db457a
pcaiozzo <paigecaiozzo@gmail.com>
2022-06-05 20:24:18 -0400

    trying to fix badges

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -8,22 +8,31 @@ function renderLicenseBadge(license) {
   let badge = ``;
   switch (license) {
     case "Apache License 2.0":
-      badge = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+      badge = `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
       break;
 
-          case 'MIT':
-      badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+    case "MIT":
+      badge =
+        "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
       break;
 
-    case 'ISC':
-      badge = '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)'
+    case "ISC":
+      badge =
+        "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
       break;
 
-      case 'GNU GPL v3':
-      badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+    case "GNU GPL v3":
+      badge =
+        "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
       break;
-  
+
+    case "Mozilla":
+      badge =
+        "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+      break;
+
     default:
+      badge = "";
       break;
   };
   return badge
@@ -68,7 +77,7 @@ ${data.usage}
 
 
 ## License
-This project is licensed using the ${data.license} license. 
+This project is licensed using the ${renderLicenseBadge(data.license)} license. 
 
 
 ## Badges
@@ -84,8 +93,9 @@ ${data.tests}
 
 ## Questions
 If you have any questions, contact me!
-GitHub: [GitHub!](https://github.com/${data.github})
-Email:  ${data.email}
+
+- GitHub: [GitHub!](https://github.com/${data.github})
+- Email:  ${data.email}
 
 
 `;
